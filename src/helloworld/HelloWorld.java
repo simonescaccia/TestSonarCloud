2b82b38d03ec0e252a12203de7d944ab9b09da6e
simonescaccia <72872543+simonescaccia@users.noreply.github.com>
2020-10-29 11:50:37 +0100

    Remove code smell System.out

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -1,7 +1,15 @@
 package helloworld;
 
-public class HelloWorld {
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+public class HelloWorld{
+	
+	private static Logger logger = Logger.getLogger("HelloWorld");
+	
 	public static void main(String[] args) {
-		System.err.println("Hello Travis CI!!");
+		
+		logger.log(Level.INFO, "Hello Travis CI!!");
+	
 	}
 }
